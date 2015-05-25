bdff8143cea2ea52b0c420c06922d8ba891f22be
TK_Ratina <taka4.kato@gmail.com>
2015-05-25 09:44:44 +0900

    learnyounode 06module

--- learnyounode/05ls/program.js
+++ learnyounode/05ls/program.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var target = process.argv[3];
-//先頭文字を検索
+// 先頭文字を検索
 if(target.match(/^\./)){
 	// console.log("ドットがある"+target);	
 }else{
@@ -9,10 +9,10 @@ if(target.match(/^\./)){
 	// console.log('add dot '+target);
 }
 
-fs.readdir(process.argv[2], function(err, list){
-	for(var i = 0; i < list.length; i++){
-		if(path.extname(list[i]) == target){
-	    	console.log(list[i]);
+fs.readdir(process.argv[2], function(err, arrlist){
+	for(var i = 0; i < arrlist.length; i++){
+		if(path.extname(arrlist[i]) == target){
+	    	console.log(arrlist[i]);
 	    }
 	}
 	
