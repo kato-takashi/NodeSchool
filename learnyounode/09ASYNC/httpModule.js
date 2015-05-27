8989cddde32b08a3247f18abe00458ded53dad06
TK_Ratina <taka4.kato@gmail.com>
2015-05-27 19:45:53 +0900

    09 途中

--- learnyounode/09ASYNC/httpModule.js
+++ learnyounode/09ASYNC/httpModule.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 // var url = "http://api.b.st-hatena.com/entry.counts?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&url=http%3A%2F%2Fb.hatena.ne.jp%2F"
-var url1 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F',
-	url2 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&callback=MyCallbackFunction',
-	url3 = 'http://api.b.st-hatena.com/entry.counts?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&url=http%3A%2F%2Fb.hatena.ne.jp%2F';
+// var url1 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F',
+// 	url2 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&callback=MyCallbackFunction',
+// 	url3 = 'http://api.b.st-hatena.com/entry.counts?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&url=http%3A%2F%2Fb.hatena.ne.jp%2F';
 
 // module.exports = function (url) {
 // 	http.get(url, function(res){
@@ -22,47 +22,72 @@ var url1 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.n
 // 	});
 // }
 
-var strArr = [];
+module.exports = function (url1, url2, url3) {
+var urlArr = [url1, url2, url3];
+var resArr = [];
+var urlNum =urlArr.length;
 
-function getHttp(url){
-	http.get(url, function(res){
-		var body = '';
-		res.setEncoding('utf8');
-		res.on('data', function(chunk){
-			body += chunk;
+	function getHttp(int){
+		http.get(urlArr[int], function(res){
+			var body = '';
+			res.setEncoding('utf8');
+			// console.log(urlArr[int]);
+
+			res.on('data', function(chunk){
+				body += chunk;
+				
+				// console.log(resArr);
+			});
+
+			res.on('end', function(res){
+				// ret = JSON.parse(body);
+				// console.log(body);
+				resArr[int] = body;
+
+				if(resArr.length === urlNum){
+					// resArr.forEach(function(num){
+					//     console.log(num);
+					// });
+					printResults();
+				}
+			});
+		}).on('error', function(e){
+			console.log('errorです。' + e.message); //エラー時
 		});
+		
+	}
 
-		res.on('end', function(res){
-			// ret = JSON.parse(body);
-			// console.log(body);
-			strArr.push(body);
-			// console.log(strArr);
-			
-		});
-	}).on('error', function(e){
-		console.log(e.message); //エラー時
-	});
-	
+	for(var i = 0; i < urlNum; i++){
+		getHttp(i);
+	}
+
+	function printResults () {
+	  for (var i = 0; i < urlNum; i++)
+		    console.log(resArr[i])
+		}
 }
 
-function three (callback){
-	getHttp(url1);
-	callback();
-	console.log(3);
-}
+// function three (callback){
+// 	getHttp(urlArr[0]);
+// 	// console.log(3);
+// 	callback();
+// }
 
-function two(callback){
-	return function(){ 
-		getHttp(url2);
-		callback(); 
-		console.log(2);
-	};	
-}
+// function two(callback){
+// 	return function(){ 
+// 		getHttp(urlArr[1]);
+// 		// console.log(2);
+// 		callback(); 
+// 	};	
+// }
 
-function one(){		
-	return function(){ 
-		getHttp(url3);
-		console.log(1);
-	};
-}
-three(two(one()));
+// function one(){		
+// 	return function(){ 
+// 		getHttp(urlArr[2]);
+// 		// console.log(1);
+// 	};
+// }
+
+// three(two(one()));
+
+
--- learnyounode/09ASYNC/program.js
+++ learnyounode/09ASYNC/program.js
@@ -1,9 +1,69 @@
 
 var httpModule = require('./httpModule.js');
-var url1 = process.argv[2],
-	url2 = process.argv[3],
-	url3 = process.argv[4];
+// var url1 = process.argv[2],
+// 	url2 = process.argv[3],
+// 	url3 = process.argv[4];
+var url1 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F',
+	url2 = 'http://api.b.st-hatena.com/entry.count?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&callback=MyCallbackFunction',
+	url3 = 'http://api.b.st-hatena.com/entry.counts?url=http%3A%2F%2Fwww.hatena.ne.jp%2F&url=http%3A%2F%2Fb.hatena.ne.jp%2F';
 
+var urlArr = [url1, url2, url3];
 httpModule(url1, url2, url3);
+/////////
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
+
+// function printResults () {
+//   for (var i = 0; i < 3; i++)
+//     console.log(results[i])
+// }
+
+// function httpGet (index) {
+//   http.get(urlArr[index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err)
+//         return console.error(err)
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count == 3)
+//         printResults()
+//     }))
+//   })
+// }
+
+// for (var i = 0; i < 3; i++)
+//   httpGet(i)
+/////////
 
 /////正解
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
+
+// function printResults () {
+//   for (var i = 0; i < 3; i++)
+//     console.log(results[i])
+// }
+
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err)
+//         return console.error(err)
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count == 3)
+//         printResults()
+//     }))
+//   })
+// }
+
+// for (var i = 0; i < 3; i++)
+//   httpGet(i)
